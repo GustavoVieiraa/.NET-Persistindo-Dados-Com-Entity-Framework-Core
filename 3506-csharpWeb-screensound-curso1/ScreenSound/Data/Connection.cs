ac11042b57cd7bc13e3699f2045ff160405a0354
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-17 20:28:59 -0300

    feat: Estruturando o ArtistaDAL para segregar a conexão com o banco referente ao Artista a tabela.

--- 3506-csharpWeb-screensound-curso1/ScreenSound/Data/Connection.cs
+++ 3506-csharpWeb-screensound-curso1/ScreenSound/Data/Connection.cs
@@ -17,30 +17,5 @@ namespace ScreenSound.Data
             return new SqlConnection(connectionString);
         }
 
-        public IEnumerable<Artista> Listar()
-        {
-            var lista = new List<Artista>();
-            using var connection = ObterConexao();
-            connection.Open();
-
-            string sql = "SELECT * FROM Artistas";
-
-            SqlCommand command = new SqlCommand(sql, connection);
-
-            using SqlDataReader dataReader = command.ExecuteReader();
-
-            while (dataReader.Read())
-            {
-                string nomeArtista = Convert.ToString(dataReader["Nome"]);
-                string bioArtista = Convert.ToString(dataReader["Bio"]);
-                int idArtista = Convert.ToInt32(dataReader["Id"]);
-                Artista artista = new Artista(nomeArtista, bioArtista) { Id = idArtista };
-
-                lista.Add(artista);
-            }
-
-            return lista;
-
-        }
     }
 }
--- 3506-csharpWeb-screensound-curso1/ScreenSound/Program.cs
+++ 3506-csharpWeb-screensound-curso1/ScreenSound/Program.cs
@@ -4,8 +4,11 @@ using ScreenSound.Modelos;
 
 try
 {
-    var connection = new Connection();
-    var listaArtistas = connection.Listar();
+    var artistaDAL = new ArtistaDAL();
+
+    artistaDAL.Adicionar(new Artista("Michael Jackson", "Michael Joseph Jackson foi um cantor, compositor, dançarino e filantropo estadunidense. Apelidado de \"Rei do Pop\", ele é considerado uma das figuras culturais mais significantes do século XX e um dos maiores artistas da história da música."));
+
+    var listaArtistas = artistaDAL.Listar();
 
     foreach (var artista in listaArtistas )
     {
