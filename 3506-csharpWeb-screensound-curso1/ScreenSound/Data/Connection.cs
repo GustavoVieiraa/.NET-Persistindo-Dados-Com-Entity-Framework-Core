fdeed4d76ad07bd28e5d603398e2c3c612958d96
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-17 20:07:29 -0300

    feat: Criado o banco de dados no SQL Server e Conectado ao DB.

---
+++ 3506-csharpWeb-screensound-curso1/ScreenSound/Data/Connection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Data
+{
+    internal class Connection
+    {
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;Encrypt=False;TrustServerCertificate=False";
+
+        public SqlConnection ObterConexao()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
