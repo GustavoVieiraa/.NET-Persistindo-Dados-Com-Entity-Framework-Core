6c5b0e934ab0706515dd355bcd89599fbc64a470
Gustavo Vieira <gustavieiradev@gmail.com>
2025-04-18 00:00:08 -0300

    Transferido todos os métodos previamente utilizados no ArtistaDAL, que seriam repetidos no MusicaDAL, para o DAL principal, o DAL genérico.
    Ademais, aprofundamos nosso entendimento na biblioteca Entity, que disponibiliza o método set, permitindo a definição do tipo genérico e sua utilização no context.

--- 3506-csharpWeb-screensound-curso1/ScreenSound/Data/ArtistaDAL.cs
+++ 3506-csharpWeb-screensound-curso1/ScreenSound/Data/ArtistaDAL.cs
@@ -5,37 +5,8 @@ namespace ScreenSound.Data
 {
     internal class ArtistaDAL: DAL<Artista>
     {
-
-        private readonly ScreenSoundContext context;
-
-        public ArtistaDAL(ScreenSoundContext context)
-        {
-            this.context = context;
-        }
-
-        public override IEnumerable<Artista> Listar()
-        {
-            return context.Artistas.ToList();   
-        }
-
-        public override void Adicionar(Artista artista)
-        {
-            context.Add(artista);
-            context.SaveChanges();
-        }
-
-        public override void Atualizar(Artista artista)
-        {
-            context.Update(artista);
-            context.SaveChanges();
-        }
-
-        public override void Deletar(Artista artista)
-        {
-            context.Remove(artista);
-            context.SaveChanges();
-        }
-
+        public ArtistaDAL(ScreenSoundContext context): base(context) { }
+    
         public Artista? RecuperarPeloNome(string nome)
         {
             return context.Artistas.FirstOrDefault(a => a.Nome.Equals(nome));
